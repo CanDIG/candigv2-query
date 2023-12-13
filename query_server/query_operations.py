99edf8c12cd86843d469557d3e6ed693d0a50364
fnguyen <OrdinaryNeumann@gmail.com>
2023-12-13 14:05:15 -0500

    Add fixes for things that changed with the Katsu UUID fix

--- query_server/query_operations.py
+++ query_server/query_operations.py
@@ -276,13 +276,13 @@ def query(treatment="", primary_site="", chemotherapy="", immunotherapy="", horm
     ret_programs = [donor['program_id'] for donor in donors[(page*page_size):((page+1)*page_size)]]
     full_data = {'results' : []}
     if len(donors) > 0:
-        params = {
-            'page_size': page_size,
-            'donors': ','.join(ret_donors)
-        }
-        r = requests.get(f"{config.KATSU_URL}/v2/authorized/donor_with_clinical_data/?{urllib.parse.urlencode(params)}",
-            headers=request.headers)
-        full_data = safe_get_request_json(r, 'Katsu donor clinical data')
+        for i, donor_id in enumerate(ret_donors):
+            donor_id_url = urllib.parse.quote(donor_id)
+            program_id_url = urllib.parse.quote(ret_programs[i])
+            print('asdf')
+            r = requests.get(f"{config.KATSU_URL}/v2/authorized/donor_with_clinical_data/program/{program_id_url}/donor/{donor_id_url}",
+                headers=request.headers)
+            full_data['results'].append(safe_get_request_json(r, 'Katsu donor clinical data'))
     else:
         full_data = {'results': []}
     full_data['genomic'] = genomic_query
