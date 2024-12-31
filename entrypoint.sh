a61f6e1fb07cbeb8c2ddd32925f057f2054b5756
fnguyen <OrdinaryNeumann@gmail.com>
2024-12-31 11:09:32 -0500

    Switch Query from Alpine to Debian

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ -f "initial_setup" ]]; then
-    sed -i s@\<HTSGET_URL\>@$CANDIG_HTSGET_URL@ config.ini
-    sed -i s@\<KATSU_URL\>@$CANDIG_KATSU_URL@ config.ini
-    sed -i s@\<OPA_URL\>@$OPA_URL@ config.ini
-
-    rm initial_setup
-fi
+sed -i s@\<HTSGET_URL\>@$CANDIG_HTSGET_URL@ config.ini
+sed -i s@\<KATSU_URL\>@$CANDIG_KATSU_URL@ config.ini
+sed -i s@\<OPA_URL\>@$OPA_URL@ config.ini
 
 cd query_server
 gunicorn -k uvicorn.workers.UvicornWorker server:app
