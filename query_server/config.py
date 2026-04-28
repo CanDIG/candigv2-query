4dd0f5bc841c4fc2291d4c20bf2b044d14984ffb
Daisie Huang <daisieh@gmail.com>
2026-04-28 13:43:15 -0700

    fix query_url

--- query_server/config.py
+++ query_server/config.py
@@ -10,7 +10,7 @@ config = configparser.ConfigParser(interpolation=None)
 config.read(os.path.abspath(f"{os.path.dirname(os.path.realpath(__file__))}/../config.ini"))
 
 AUTHZ = config['authz']
-QUERY_URL = os.getenv("QUERY_URL", f"http://localhost:{config['DEFAULT']['Port']}")
+QUERY_URL = os.getenv("CANDIG_QUERY_URL", f"http://localhost:{config['DEFAULT']['Port']}")
 AGGREGATE_COUNT_THRESHOLD = int(os.getenv("AGGREGATE_COUNT_THRESHOLD", "5"))
 
 PORT = config['DEFAULT']['Port']
